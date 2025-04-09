1e6d34ad26d59cdb9c2c52c8c55729f4e5cddb70
Viswanatha Reddy <vreddy+eqbank@eqbank.ca>
2025-04-08 20:24:07 -0400

    Inital Commit

---
+++ src/main/java/org/example/demoactions/DemoActionsApplication.java
@@ -0,0 +1,13 @@
+package org.example.demoactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoActionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/demoactions/DemoActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demoactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
